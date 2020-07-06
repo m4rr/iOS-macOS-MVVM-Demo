61c17b5abc3aba92c32ed68fd60ff2a5095cd1c9
Marat Saytakov <remarr@gmail.com>
2020-07-06 15:04:36 +0530

    add SwiftUI app target

---
+++ Shared/PhotoLibSwiftUIApp.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoLibSwiftUIApp.swift
+//  Shared
+//
+//  Created by Marat Say on 7/6/20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLibSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+struct PhotoLibSwiftUIApp_Previews: PreviewProvider {
+  static var previews: some View {
+    /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+  }
+}
